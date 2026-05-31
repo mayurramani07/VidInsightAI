30ad2078421eb976f16f486ed85b9ae7b41574d0
Mayur Ramani <ramanimayur836@gmail.com>
2026-05-31 22:58:34 +0530

    feat(llm): add reusable LCEL chain builder for Mistral prompts

---
+++ core/extractor.py
@@ -0,0 +1,20 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+import os 
+
+
+def get_llm():
+    return ChatMistralAI(model = "mistral-small-latest", mistral_api_key = os.getenv("MISTRAL_API_KEY"),temperature=0.2)
+
+
+
+def build_chain(system_prompt : str):
+    llm = get_llm()
+    return (
+        RunnablePassthrough() | RunnableLambda(lambda x : {"text" : x}) |ChatPromptTemplate.from_messages([
+        ("system", system_prompt),
+        ("human","{text}"),
+    ]) | llm |StrOutputParser()
+    )
